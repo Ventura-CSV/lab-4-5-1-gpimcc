1f6805933dd8b0108ac38f3b76c98827a3fef0dd
Gabriella <gabriella_pimentel1@my.vcccd.edu>
2024-06-26 13:08:42 -0400

    completed lab

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ def main():
     Code Your Program here
     ########################################
     """
+    numbers = [] 
+    while len(numbers) < 5:
+        randnum = random.randint(0,100)
+        numbers.append(randnum)
+    total = 0
+    for number in numbers:
+        total += number
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
